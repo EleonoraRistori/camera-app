65bc4110010695f8d33718bc838f597c539cc0e8
EleonoraRistori <62255811+EleonoraRistori@users.noreply.github.com>
2022-08-23 19:38:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function placeButton() {
+    var cameraView = document.getElementById("camera--view")
+    x_pos = Math.floor(Math.random() * (cameraView.offsetWidth + 1))
+    y_pos = Math.floor(Math.random() * (cameraView.offsetHeight + 1))
+    var d = document.getElementById('camera--trigger');
+    d.style.position = "absolute";
+    d.style.left = x_pos+'px';
+    d.style.top = y_pos+'px';
+    d.style.width = Math.floor(Math.random() * (200))+'px';
+    d.style.height = Math.floor(Math.random() * (200))+'px';
+}
+
+placeButton()
