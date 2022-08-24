9ec6671adf94409b33945b53adaf4468d8cf7fdb
EleonoraRistori <62255811+EleonoraRistori@users.noreply.github.com>
2022-08-24 13:12:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,5 +9,26 @@ function placeButton() {
     d.style.width = Math.floor(Math.random() * (200))+'px';
     d.style.height = Math.floor(Math.random() * (200))+'px';
 }
-const interval = setInterval(placeButton, 500);
 
+
+
+function detailsButtons() {
+    var cameraView = document.getElementById("camera--view")
+    for(let i = 0; i < 4 ; i++){
+        var e= document.getElementById('detail'+i);
+        e.style.display = 'none';
+    }
+    for(let i = 0; i < Math.random() * (4) ; i++){
+        var el= document.getElementById('detail'+i);
+        x_pos = Math.floor(Math.random() * (cameraView.offsetWidth + 1))
+        y_pos = Math.floor(Math.random() * (cameraView.offsetHeight + 1))
+        el.style.position = "absolute";
+        el.style.left = x_pos+'px';
+        el.style.top = y_pos+'px';
+        el.style.width = Math.floor(Math.random() * (200))+'px';
+        el.style.height = Math.floor(Math.random() * (200))+'px';
+        el.style.display = 'block';
+    }
+}
+
+const interval = setInterval(detailsButtons, 500);
